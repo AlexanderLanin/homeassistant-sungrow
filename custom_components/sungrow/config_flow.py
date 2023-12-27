2900462187aed0d5c4c8355b00e6412940a08936
Alexander Lanin <alex@lanin.de>
2023-12-27 23:24:43 +0100

    Keep previously entered data in case of errors during config

--- custom_components/sungrow/config_flow.py
+++ custom_components/sungrow/config_flow.py
@@ -22,24 +22,6 @@ from .inverter import connect_inverter
 
 logger = logging.getLogger(__name__)
 
-# This is the schema that used to display the UI to the user.
-# TODO add CONF_SCAN_INTERVAL
-DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_HOST): str,
-    vol.Required(CONF_PORT, default=8082): int,
-    vol.Required(CONF_TIMEOUT, default=3): int,
-    vol.Required(CONF_SLAVE, default=0x01): int,
-    vol.Required("connection", default='http'): selector({
-        "select": {
-            "options": ["http", "modbus", "sungrow"],
-        }
-    }),
-    vol.Optional("model"): str,
-    vol.Optional("use_local_time"): bool,
-    vol.Optional("smart_meter"): bool,
-    vol.Optional("level", default=2): int
-})
-
 
 class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
     """Sungrow Inverter config flow."""
@@ -56,11 +38,6 @@ class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
         # Accumulate validation errors. Key is name of field from DATA_SCHEMA
         errors = {}
 
-        # Don't do anything if we don't have a configuration
-        if not config_inverter:
-            logger.debug(f'validate_input returning None due to no config')
-            return None
-
         # Validate the data can be used to set up a connection.
         logger.debug(f'validate_input creating SungrowInverter')
         is_success, inverter = await hass.async_add_executor_job(connect_inverter(config_inverter))
@@ -74,6 +51,45 @@ class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return (errors, inverter)
 
+    async def _async_show_user_form(
+        self, user_input: dict[str, Any] = {}, errors: dict = {}
+    ):
+        logger.debug(
+            "async_step_user displaying user data entry form "
+            + f"with user_input={user_input} and errors={errors}"
+        )
+
+        schema = {
+            vol.Required(CONF_HOST, default=user_input.get(CONF_HOST, "")): str,
+            vol.Required(CONF_PORT, default=user_input.get(CONF_PORT, 8082)): int,
+            vol.Required(CONF_TIMEOUT, default=user_input.get(CONF_TIMEOUT, 3)): int,
+            vol.Required(CONF_SLAVE, default=user_input.get(CONF_SLAVE, 1)): int,
+            vol.Required(
+                "connection", default=user_input.get("connection", "http")
+            ): selector(
+                {
+                    "select": {
+                        "options": ["http", "modbus", "sungrow"],
+                    }
+                }
+            ),
+            vol.Optional("model", default=user_input.get("model", "")): str,
+            vol.Optional(
+                "use_local_time",
+                default=user_input.get("use_local_time", False),
+            ): bool,
+            vol.Optional(
+                "smart_meter", default=user_input.get("smart_meter", False)
+            ): bool,
+            vol.Optional("level", default=user_input.get("level", 2)): int,
+        }
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(schema),
+            errors=errors,
+        )
+
     async def async_step_user(self, user_input=None):
         """Initial configuration step
         Either show config data entry form to the user, or create a config entry.
@@ -83,8 +99,7 @@ class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
 
         # Either show modal form, or create config entry then move on
         if not user_input: # Just show the modal form and return if no user input
-            logger.debug('async_step_user displaying user data entry form')
-            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+            return await self._async_show_user_form()
         else: # We got user input, so do something with it
             # Validate inputs and do a test connection/scrape of the inverter
             # Both info and errors are None when config flow is first invoked
@@ -92,7 +107,7 @@ class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
             logger.debug(f'async_step_user errors={pformat(errors)}')
 
             # Either display errors in form, or create config entry and close form
-            if not errors or not len(errors.keys()):
+            if not errors:
                 # Figure out a unique id (that never changes!) for the device
                 unique_device_id = inverter.latest_scrape.get('serial_number')
                 logger.debug(f'async_step_user assigning unique_id {unique_device_id}')
@@ -106,8 +121,4 @@ class SungrowInverterConfigFlow(ConfigFlow, domain=DOMAIN):
             else:
                 # If there is no user input or there were errors, show the form again,
                 # including any errors that were found with the input.
-                logger.debug(
-                    f'async_step_user calling async_show_form step_id="user"')
-                return self.async_show_form(
-                    step_id="user", data_schema=DATA_SCHEMA, errors=errors
-                )
+                return await self._async_show_user_form(user_input, errors)
